fa422e22af8b3f99f6120028f8a4b3132922bc9b
Felipe <felipejm9805@gmail.com>
2020-05-03 14:28:40 -0700

    Creacion de modulos

---
+++ instance/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+  default=""
+  description="Ami id"
+}
+
+variable "instance_type" {
+
+}
+
+variable "tags" {
+    type = map
+}
+
+variable "sg_name" {
+  
+}
+
+variable "ingress_rules" {
+  
+}
